74e90186501c4945015f8eab5524eb5c59596224
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-01 17:55:04 +0700

    tambah opsi menu 0 untuk keluar

--- tugas_looping.cpp
+++ tugas_looping.cpp
@@ -7,7 +7,9 @@ int pilihan;
 void tampilkanMenu() { 
     cout << "\n========== MENU UTAMA ==========" << endl;
 cout << "1. Cek Bilangan Prima" << endl;
-    cout << "2. Cek Bilangan Fibonacci" << endl;   
+    cout << "2. Cek Bilangan Fibonacci" << endl;  
+    cout << "0. Keluar" << endl;
+    cout << "Pilih menu: "; 
 }
 
 int main() {
