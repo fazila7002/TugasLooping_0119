47901f1a807ed4ce62ff51e1218e6a458e02e76e
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-01 17:54:25 +0700

    opsi menu 1 dan 2

--- tugas_looping.cpp
+++ tugas_looping.cpp
@@ -6,7 +6,9 @@ int pilihan;
 
 void tampilkanMenu() { 
     cout << "\n========== MENU UTAMA ==========" << endl;
-   }
+cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;   
+}
 
 int main() {
     return 0;
